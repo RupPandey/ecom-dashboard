415619ce5888cd68167736b1be66e15fc072744f
unknown <errup.pandey19@gmail.com>
2022-11-16 04:26:12 +0530

    first commit

---
+++ ecom-dashboar/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ECom Dashboard</h1>
+      <button>Submit</button><br /> <br />
+      <Button>Submit Button</Button>
+    </div>
+  );
+}
+
+export default App;
